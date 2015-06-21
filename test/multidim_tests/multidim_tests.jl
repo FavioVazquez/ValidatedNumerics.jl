f3b8711c7368df05cf86ae4b299531e005ce9a5c
David P. Sanders <dpsanders@gmail.com>
2015-06-21 11:03:33 -0500

    Added multidim_tests

---
+++ test/multidim_tests/multidim_tests.jl
@@ -0,0 +1,27 @@
+
+using ValidatedNumerics
+using FactCheck
+
+
+A = [2 1; 1 1]
+
+f(x) = A * x
+
+function henon(xx)
+    a = 1.4
+    b = 0.3
+
+    x, y = xx
+
+    [1 - a*x^2 + y, b*x]
+end
+
+unit_interval = @interval(0, 1)
+unit_square = [unit_interval, unit_interval]  # replace by constructor call in 0.4
+
+facts("Image of unit square test") do
+    @fact f(unit_square) => [ @interval(0, 3), @interval(0, 2) ]
+
+    @fact henon(unit_square) =>[ Interval(-0.40000000000000013, 2.0), Interval(0.0, 0.30000000000000004)]
+end
+
